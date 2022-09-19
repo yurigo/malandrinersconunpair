754c8904d8b6999b773d80a7105537ec33ef4e18
Yuri <ygonzoo@gmail.com>
2022-09-19 23:32:02 +0200

    Add iteracion 2 y 3
    - passwordAdminStrengthChecker,
    - passwordSpecialCharacterChecker
    - passwordStrengthCheckerFeedback

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-export function passwordLengthChecker(password) {
-    return password.length >= 7;
+export function passwordLengthChecker(password, minLength = 7) {
+    return password.length >= minLength;
 }
 
 export function passwordLetterChecker(password) {
@@ -16,4 +16,37 @@ export function passwordStrengthChecker(password) {
     return passwordLengthChecker(password) &&
         passwordLetterChecker(password) &&
         passwordDigitChecker(password)
+}
+
+export function passwordSpecialCharacterChecker(password) {
+    return /[\W]/.test(password);
+}
+
+export function passwordAdminStrengthChecker(password) {
+    if (!password) return false;
+
+    return passwordStrengthChecker(password)
+        && passwordLengthChecker(password, 10)
+        && passwordSpecialCharacterChecker(password)
+}
+
+export function passwordStrengthCheckerFeedback(password) {
+
+
+    if (!password)
+        return ["Va, venga, ponme un password..."]
+
+    const listOfErrors = [];
+
+    if (!passwordLengthChecker(password))
+        listOfErrors.push("El password debe tener más de 7 carácteres")
+
+    if (!passwordLetterChecker(password))
+        listOfErrors.push("El password debe tener al menos una letra")
+
+    if (!passwordDigitChecker(password))
+        listOfErrors.push("El password debe tener al menos un número")
+
+
+    return listOfErrors
 }
--- test.js
+++ test.js
@@ -6,7 +6,10 @@ import {
     passwordLengthChecker,
     passwordDigitChecker,
     passwordLetterChecker,
-    passwordStrengthChecker
+    passwordStrengthChecker,
+    passwordAdminStrengthChecker,
+    passwordSpecialCharacterChecker,
+    passwordStrengthCheckerFeedback
 } from "./main.js";
 
 test('mi primer test', (t) => {
@@ -55,3 +58,84 @@ test('Iteracion 1', async (t) => {
 
 })
 
+test('Iteracion 2', async (t) => {
+    await t.test('Admin passwords must be at least 10 characters in length', (t) => {
+        assert.strictEqual(passwordLengthChecker("abcde", 10), false);
+        assert.strictEqual(passwordLengthChecker("12345657890", 10), true);
+        assert.strictEqual(passwordLengthChecker("12345657", 10), false);
+        assert.strictEqual(passwordLengthChecker("12345", 10), false);
+    });
+
+    await t.test('Admin passwords must contain at least 1 special character', (t) => {
+        assert.strictEqual(passwordSpecialCharacterChecker("abcde"), false);
+        assert.strictEqual(passwordSpecialCharacterChecker("?"), true);
+        assert.strictEqual(passwordSpecialCharacterChecker(""), false);
+        assert.strictEqual(passwordSpecialCharacterChecker("."), true);
+    });
+
+    await t.test('Integracion', () => {
+        assert.strictEqual(passwordAdminStrengthChecker(), false);
+        assert.strictEqual(passwordAdminStrengthChecker("abcde"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("1234565789"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("12345657"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("12345"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("a"), false);
+        assert.strictEqual(passwordAdminStrengthChecker(""), false);
+        assert.strictEqual(passwordAdminStrengthChecker("6"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("LoremIpsumDolorSitAmet"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("LoremIpsumDolorSitAmet54"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("109784165071a"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("1010aa"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("1010aa.er398"), true);
+        assert.strictEqual(passwordAdminStrengthChecker("101.e38"), false);
+        //adding iteration 1 restrictions
+        assert.strictEqual(passwordAdminStrengthChecker("101033.88398"), false);
+        assert.strictEqual(passwordAdminStrengthChecker("···············"), false);
+    });
+
+})
+
+test('Iteracion 3', async (t) => {
+    await t.test("Provide the user with a list of reasons why their password is 'weak'", (t) => {
+
+        // assert.strictEqual(passwordStrengthCheckerFeedback("abcde"), feedback1);
+
+        // console.log(passwordStrengthCheckerFeedback("abcde"))
+
+        assert.strictEqual(passwordStrengthCheckerFeedback("abcde").length, 2);
+
+        // test agnostico al orden (.sort())
+        const feedback1 = [
+            "El password debe tener más de 7 carácteres",
+            "El password debe tener al menos un número"
+        ]
+        assert.deepEqual(passwordStrengthCheckerFeedback("abcde").sort(), feedback1.sort());
+
+        const feedback2 = [
+            "El password debe tener al menos un número",
+            "El password debe tener más de 7 carácteres",
+        ]
+        assert.deepEqual(passwordStrengthCheckerFeedback("abcde").sort(), feedback2.sort());
+
+
+        const feedbackSinPassword = [
+            "Va, venga, ponme un password..."
+        ]
+
+        assert.deepEqual(passwordStrengthCheckerFeedback().sort(), feedbackSinPassword.sort());
+
+
+        const feedback3 = [
+            "El password debe tener más de 7 carácteres",
+            "El password debe tener al menos una letra",
+            "El password debe tener al menos un número"
+        ]
+
+        assert.deepEqual(passwordStrengthCheckerFeedback(".").sort(), feedback3.sort());
+
+        const feedback4 = []
+
+        assert.deepEqual(passwordStrengthCheckerFeedback("LoremIpsumDolorSitAmet3").sort(), feedback4.sort());
+
+    });
+})
